7bc46733ecf519a4f31a10bfaee4a62cfcd5df8f
fil7er <fil7er@gmail.com>
2022-05-31 12:31:50 -0400

    Update prices.mjs

--- src/prices.mjs
+++ src/prices.mjs
@@ -87,7 +87,6 @@ function createApp(database) {
       let holiday2 = Temporal.PlainDate.from(row.holiday);
       if (
         date && date2.equals(holiday2) &&
-        date.getFullYear() === holiday.getFullYear() &&
         date.getMonth() === holiday.getMonth() &&
         date.getDate() === holiday.getDate()
       ) {
