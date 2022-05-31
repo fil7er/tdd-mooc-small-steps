aee714cf68ad00f06f93be62f0e116d92679ae1a
fil7er <fil7er@gmail.com>
2022-05-31 12:44:03 -0400

    Update prices.mjs

--- src/prices.mjs
+++ src/prices.mjs
@@ -20,7 +20,7 @@ function createApp(database) {
     const baseCost = database.findBasePriceByType(type).cost;
     const date = parseDate(req.query.date);
     const date2 = parsePlainDate(req.query.date);
-    const cost = calculateCost(age, type, date, baseCost);
+    const cost = calculateCost(age, type, baseCost, date2);
     res.json({ cost });
   });
 
@@ -30,7 +30,7 @@ function createApp(database) {
     }
   }
 
-  function calculateCost(age, type, date, baseCost, date2) {
+  function calculateCost(age, type, baseCost, date2) {
     if (type === "night") {
       return calculateCostForNightTicket(age, baseCost, date2);
     } else {
